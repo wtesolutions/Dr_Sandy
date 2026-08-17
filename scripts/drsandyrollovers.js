49391f71bae9a4bad940a20962d714cf1f2fb715
Ralph_Fonz <ralph.fonz@gmail.com>
2026-08-17 16:41:35 -0400

    Fix hardcoded absolute image paths inside rollover JS
    drsandyrollovers.js preloads nav rollover images via hardcoded
    /images/doctor/... paths, which 404 under the GitHub Pages project
    subpath for the same reason the HTML attributes did. The file is
    shared by both root-level pages and one-level-deep pages (doctor/,
    sexypage/), so a single relative prefix can't fit both: rewrote
    scripts/drsandyrollovers.js (used by the depth-1 pages) to use
    ../images/... and default_files/drsandyrollovers.js (used by root
    pages) to use images/..., and repointed the 3 root pages that had
    been including the depth-1 copy to use the root-relative one instead.

--- scripts/drsandyrollovers.js
+++ scripts/drsandyrollovers.js
@@ -31,36 +31,36 @@ else br = "n2";
 if (br== "n3") 
 
 {
-imgAboutUson = new Image();imgAboutUson.src = "/images/doctor/drsandy_about_on.jpg"; imgAboutUsoff = new Image(); imgAboutUsoff.src = "/images/doctor/drsandy_about_off.jpg"; 
-imgContactUson = new Image();imgContactUson.src = "/images/doctor/drsandy_contact_on.jpg"; imgContactUsoff = new Image(); imgContactUsoff.src = "/images/doctor/drsandy_contact_off.jpg"; 
-imgIntimacyDron = new Image();imgIntimacyDron.src = "/images/doctor/drsandy_intdr_on.jpg"; imgIntimacyDroff = new Image(); imgIntimacyDroff.src = "/images/doctor/drsandy_intdr_off.jpg"; 
-imgSexEncycon = new Image();imgSexEncycon.src = "/images/doctor/drsandy_sexencyc_on.jpg"; imgSexEncycoff = new Image(); imgSexEncycoff.src = "/images/doctor/drsandy_sexencyc_off.jpg"; 
-imgShopon = new Image();imgShopon.src = "/images/doctor/drsandy_shop_on.jpg"; imgShopoff = new Image(); imgShopoff.src = "/images/doctor/drsandy_shop_off.jpg"; 
-imgDrsandySIQon = new Image();imgDrsandySIQon.src = "/images/doctor/drsandy_SIQ_on.jpg"; imgDrsandySIQoff = new Image(); imgDrsandySIQoff.src = "/images/doctor/drsandy_SIQ_off.jpg"; 
+imgAboutUson = new Image();imgAboutUson.src = "../images/doctor/drsandy_about_on.jpg"; imgAboutUsoff = new Image(); imgAboutUsoff.src = "../images/doctor/drsandy_about_off.jpg"; 
+imgContactUson = new Image();imgContactUson.src = "../images/doctor/drsandy_contact_on.jpg"; imgContactUsoff = new Image(); imgContactUsoff.src = "../images/doctor/drsandy_contact_off.jpg"; 
+imgIntimacyDron = new Image();imgIntimacyDron.src = "../images/doctor/drsandy_intdr_on.jpg"; imgIntimacyDroff = new Image(); imgIntimacyDroff.src = "../images/doctor/drsandy_intdr_off.jpg"; 
+imgSexEncycon = new Image();imgSexEncycon.src = "../images/doctor/drsandy_sexencyc_on.jpg"; imgSexEncycoff = new Image(); imgSexEncycoff.src = "../images/doctor/drsandy_sexencyc_off.jpg"; 
+imgShopon = new Image();imgShopon.src = "../images/doctor/drsandy_shop_on.jpg"; imgShopoff = new Image(); imgShopoff.src = "../images/doctor/drsandy_shop_off.jpg"; 
+imgDrsandySIQon = new Image();imgDrsandySIQon.src = "../images/doctor/drsandy_SIQ_on.jpg"; imgDrsandySIQoff = new Image(); imgDrsandySIQoff.src = "../images/doctor/drsandy_SIQ_off.jpg"; 
 
-imgloveprgrmon = new Image();imgloveprgrmon.src = "/images/doctor/loveprgrm-on.gif"; imgloveprgrmoff = new Image(); imgloveprgrmoff.src = "/images/doctor/loveprgrm-off.gif"; 
-imgloveprgrmsq = new Image();imgloveprgrmsq.src = "/images/doctor/loveprogram_sq.jpg";
-imglovelessonson = new Image();imglovelessonson.src = "/images/doctor/lovelessons-on.gif"; imglovelessonsoff = new Image(); imglovelessonsoff.src = "/images/doctor/lovelessons-off.gif"; 
-imglovelessonssq = new Image();imglovelessonssq.src = "/images/doctor/lovelessons_sq.jpg";
-imglovetoolson = new Image();imglovetoolson.src = "/images/doctor/lovetools-on.gif"; imglovetoolsoff = new Image(); imglovetoolsoff.src = "/images/doctor/lovetools-off.gif"; 
-imglovetoolssq = new Image();imglovetoolssq.src = "/images/doctor/lovetools_sq.jpg";
-imgprgrmtipson = new Image();imgprgrmtipson.src = "/images/doctor/prgrmtips-on.gif"; imgprgrmtipsoff = new Image(); imgprgrmtipsoff.src = "/images/doctor/prgrmtips-off.gif"; 
-imgprgrmtipssq = new Image();imgprgrmtipssq.src = "/images/doctor/programtips_sq.jpg";
-imgintimacyqaon = new Image();imgintimacyqaon.src = "/images/doctor/intimacyqa-on.gif"; imgintimacyqaoff = new Image(); imgintimacyqaoff.src = "/images/doctor/intimacyqa-off.gif"; 
-imgintimacyqasq = new Image();imgintimacyqasq.src = "/images/doctor/intimacyqa_sq.jpg";
-imgsexandhealthon = new Image();imgsexandhealthon.src = "/images/doctor/sexandhealth-on.gif"; imgsexandhealthoff = new Image(); imgsexandhealthoff.src = "/images/doctor/sexandhealth-off.gif"; 
-imgsexandhealthsq = new Image();imgsexandhealthsq.src = "/images/doctor/sexandhealth_sq.jpg";
-imglovestyleson = new Image();imglovestyleson.src = "/images/doctor/lovestyles-on.gif"; imglovestylesoff = new Image(); imglovestylesoff.src = "/images/doctor/lovestyles-off.gif"; 
-imglovestylessq = new Image();imglovestylessq.src = "/images/doctor/lovestyles_sq.jpg";
-imgaboutdrsandyon = new Image();imgaboutdrsandyon.src = "/images/doctor/aboutdrsandy-on.gif"; imgaboutdrsandyoff = new Image(); imgaboutdrsandyoff.src = "/images/doctor/aboutdrsandy-off.gif"; 
-imgaboutdrsandysq = new Image();imgaboutdrsandysq.src = "/images/doctor/aboutdrsandy_sq.jpg";
-imgherbookon = new Image();imgherbookon.src = "/images/doctor/herbook-on.gif"; imgherbookoff = new Image(); imgherbookoff.src = "/images/doctor/herbook-off.gif"; 
-imgherbooksq = new Image();imgherbooksq.src = "/images/doctor/herbook_sq.jpg";
-imghervideoon = new Image();imghervideoon.src = "/images/doctor/hervideo-on.gif"; imghervideooff = new Image(); imghervideooff.src = "/images/doctor/hervideo-off.gif"; 
-imghervideosq = new Image();imghervideosq.src = "/images/doctor/hervideo_sq.jpg";
-imgdrsandysayson = new Image();imgdrsandysayson.src = "/images/doctor/drsandysays-on.gif"; imgdrsandysaysoff = new Image(); imgdrsandysaysoff.src = "/images/doctor/drsandysays-off.gif"; 
-imgdrsandysayssq = new Image();imgdrsandysayssq.src = "/images/doctor/sandysays_sq.jpg";
-imgAddToCarton = new Image();imgAddToCarton.src = "/images/addtocartbtn_on.gif"; imgAddToCartoff = new Image(); imgAddToCartoff.src = "/images/addtocartbtn_off.gif"; 
+imgloveprgrmon = new Image();imgloveprgrmon.src = "../images/doctor/loveprgrm-on.gif"; imgloveprgrmoff = new Image(); imgloveprgrmoff.src = "../images/doctor/loveprgrm-off.gif"; 
+imgloveprgrmsq = new Image();imgloveprgrmsq.src = "../images/doctor/loveprogram_sq.jpg";
+imglovelessonson = new Image();imglovelessonson.src = "../images/doctor/lovelessons-on.gif"; imglovelessonsoff = new Image(); imglovelessonsoff.src = "../images/doctor/lovelessons-off.gif"; 
+imglovelessonssq = new Image();imglovelessonssq.src = "../images/doctor/lovelessons_sq.jpg";
+imglovetoolson = new Image();imglovetoolson.src = "../images/doctor/lovetools-on.gif"; imglovetoolsoff = new Image(); imglovetoolsoff.src = "../images/doctor/lovetools-off.gif"; 
+imglovetoolssq = new Image();imglovetoolssq.src = "../images/doctor/lovetools_sq.jpg";
+imgprgrmtipson = new Image();imgprgrmtipson.src = "../images/doctor/prgrmtips-on.gif"; imgprgrmtipsoff = new Image(); imgprgrmtipsoff.src = "../images/doctor/prgrmtips-off.gif"; 
+imgprgrmtipssq = new Image();imgprgrmtipssq.src = "../images/doctor/programtips_sq.jpg";
+imgintimacyqaon = new Image();imgintimacyqaon.src = "../images/doctor/intimacyqa-on.gif"; imgintimacyqaoff = new Image(); imgintimacyqaoff.src = "../images/doctor/intimacyqa-off.gif"; 
+imgintimacyqasq = new Image();imgintimacyqasq.src = "../images/doctor/intimacyqa_sq.jpg";
+imgsexandhealthon = new Image();imgsexandhealthon.src = "../images/doctor/sexandhealth-on.gif"; imgsexandhealthoff = new Image(); imgsexandhealthoff.src = "../images/doctor/sexandhealth-off.gif"; 
+imgsexandhealthsq = new Image();imgsexandhealthsq.src = "../images/doctor/sexandhealth_sq.jpg";
+imglovestyleson = new Image();imglovestyleson.src = "../images/doctor/lovestyles-on.gif"; imglovestylesoff = new Image(); imglovestylesoff.src = "../images/doctor/lovestyles-off.gif"; 
+imglovestylessq = new Image();imglovestylessq.src = "../images/doctor/lovestyles_sq.jpg";
+imgaboutdrsandyon = new Image();imgaboutdrsandyon.src = "../images/doctor/aboutdrsandy-on.gif"; imgaboutdrsandyoff = new Image(); imgaboutdrsandyoff.src = "../images/doctor/aboutdrsandy-off.gif"; 
+imgaboutdrsandysq = new Image();imgaboutdrsandysq.src = "../images/doctor/aboutdrsandy_sq.jpg";
+imgherbookon = new Image();imgherbookon.src = "../images/doctor/herbook-on.gif"; imgherbookoff = new Image(); imgherbookoff.src = "../images/doctor/herbook-off.gif"; 
+imgherbooksq = new Image();imgherbooksq.src = "../images/doctor/herbook_sq.jpg";
+imghervideoon = new Image();imghervideoon.src = "../images/doctor/hervideo-on.gif"; imghervideooff = new Image(); imghervideooff.src = "../images/doctor/hervideo-off.gif"; 
+imghervideosq = new Image();imghervideosq.src = "../images/doctor/hervideo_sq.jpg";
+imgdrsandysayson = new Image();imgdrsandysayson.src = "../images/doctor/drsandysays-on.gif"; imgdrsandysaysoff = new Image(); imgdrsandysaysoff.src = "../images/doctor/drsandysays-off.gif"; 
+imgdrsandysayssq = new Image();imgdrsandysayssq.src = "../images/doctor/sandysays_sq.jpg";
+imgAddToCarton = new Image();imgAddToCarton.src = "../images/addtocartbtn_on.gif"; imgAddToCartoff = new Image(); imgAddToCartoff.src = "../images/addtocartbtn_off.gif"; 
 
 bAllImagesLoaded = 1;
 } 
